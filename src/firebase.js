662169addb3c0221a21f6748734c0e14d6c78e3b
jun_ <gurwns232@naver.com>
2024-02-23 11:06:22 +0900

    refactor: 불필요한 주석 제거

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
-// import { getAnalytics } from 'firebase/analytics'
-// import { firebaseKey } from './api/key'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  // eslint-disable-next-line no-undef
   apiKey: import.meta.env.VITE_FIREBASE_KEY,
   authDomain: 'test-a42de.firebaseapp.com',
   projectId: 'test-a42de',
@@ -18,8 +10,6 @@ const firebaseConfig = {
   measurementId: 'G-6DNJFS8ETG',
 }
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig)
-// const analytics = getAnalytics(app)
 
 export default app
