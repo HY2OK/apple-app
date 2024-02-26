47cbbb5de4bd0fe274f622a1f975882d938591be
jun_ <gurwns232@naver.com>
2024-02-26 19:54:24 +0900

    feat: modal에서 detail page로 이동 버튼 생성

--- src/components/MovieModal/MovieModal.jsx
+++ src/components/MovieModal/MovieModal.jsx
@@ -3,18 +3,21 @@ import { useRef } from 'react'
 import './MovieModal.css'
 import { imageBasePath } from '../../constant'
 import useOnclickOutside from '../../hooks/useOnClickOutside'
+import { useNavigate } from 'react-router-dom'
 
 const MovieModal = ({
   backdrop_path,
   title,
   overview,
   name,
+  id,
   release_date,
   first_air_date,
   vote_average,
   setModalOpen,
 }) => {
   const ref = useRef(null)
+  const navigate = useNavigate()
 
   useOnclickOutside(ref, () => setModalOpen(false))
 
@@ -33,12 +36,27 @@ const MovieModal = ({
           />
           <div className="modal__content">
             <p className="modal__details">
-              <span>100% for you</span>
+              <span>100% for you </span>
               {release_date ? release_date : first_air_date}
             </p>
             <h2 className="modal__title">{title ? title : name}</h2>
             <p className="modal__overview">평점: {vote_average}</p>
             <p className="modal__overview">{overview}</p>
+            <button
+              style={{
+                position: 'absolute',
+                right: '30px',
+                border: 'none',
+                borderRadius: '5px',
+                outline: 'none',
+                width: '100px',
+                height: '30px',
+                cursor: 'pointer',
+              }}
+              onClick={() => navigate(`/${id}`)}
+            >
+              자세히 보기
+            </button>
           </div>
         </div>
       </div>
